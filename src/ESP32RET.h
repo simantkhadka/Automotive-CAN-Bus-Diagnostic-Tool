e54ada1b27ca1eff6b1d53709b0a16e5bf3e3cbd
Simant Khadka <khadkasimant01@gmail.com>
2025-08-06 13:58:53 +0545

    Delete src directory

--- src/ESP32RET.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-#include "esp32_can.h"
-
-void loadSettings();
-void processDigToggleFrame(CAN_FRAME &frame);
-void sendDigToggleMsg();
-void sendMarkTriggered(int which);
